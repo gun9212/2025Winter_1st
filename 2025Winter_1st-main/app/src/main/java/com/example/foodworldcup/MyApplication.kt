a564afb4ea0e44f48c3e6a1cd2070f6d740f3f74
seyoonp <seyoonp1@gmail.com>
2026-01-12 00:51:34 +0900

    remove: 키해시 얻는 부분 삭제

--- 2025Winter_1st-main/app/src/main/java/com/example/foodworldcup/MyApplication.kt
+++ 2025Winter_1st-main/app/src/main/java/com/example/foodworldcup/MyApplication.kt
@@ -1,12 +1,8 @@
 package com.example.foodworldcup
 
 import android.app.Application
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.content.pm.Signature
-import com.example.foodworldcup.BuildConfig
 import com.kakao.vectormap.KakaoMapSdk
-import java.security.MessageDigest
+import com.example.foodworldcup.BuildConfig
 
 /**
  * Application 클래스
@@ -16,9 +12,6 @@ import java.security.MessageDigest
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // 카카오맵 API 등록용 해시값 출력
-        printKakaoMapHash()
         
         // Kakao Map SDK 초기화
         // local.properties에서 KAKAO_MAP_KEY를 읽어서 초기화
@@ -37,46 +30,5 @@ class MyApplication : Application() {
             android.util.Log.e("MyApplication", "KAKAO_MAP_KEY가 설정되지 않았습니다. local.properties를 확인해주세요.")
         }
     }
-    
-    /**
-     * 카카오맵 API 등록에 필요한 해시값을 로그로 출력합니다.
-     * 카카오 개발자 콘솔(https://developers.kakao.com)에서 앱 등록 시 사용합니다.
-     */
-    private fun printKakaoMapHash() {
-        try {
-            val packageName = packageName
-            val packageInfo: PackageInfo = packageManager.getPackageInfo(
-                packageName,
-                PackageManager.GET_SIGNATURES
-            )
-            
-            android.util.Log.d("MyApplication", "========================================")
-            android.util.Log.d("MyApplication", "카카오맵 API 등록용 정보")
-            android.util.Log.d("MyApplication", "========================================")
-            android.util.Log.d("MyApplication", "패키지명: $packageName")
-            
-            val signatures = packageInfo.signatures
-            if (signatures != null) {
-                for (signature in signatures) {
-                    val md: MessageDigest = MessageDigest.getInstance("SHA")
-                    md.update(signature.toByteArray())
-                    val hash = md.digest()
-                    
-                    // SHA-1 해시를 16진수 문자열로 변환
-                    val hashString = hash.joinToString("") { "%02X".format(it) }
-                    
-                    android.util.Log.d("MyApplication", "SHA-1: $hashString")
-                    android.util.Log.d("MyApplication", "해시값 (패키지명 + SHA-1): $packageName;$hashString")
-                }
-            } else {
-                android.util.Log.w("MyApplication", "서명 정보를 가져올 수 없습니다.")
-            }
-            android.util.Log.d("MyApplication", "========================================")
-            android.util.Log.d("MyApplication", "위 해시값을 카카오 개발자 콘솔에 등록하세요.")
-            android.util.Log.d("MyApplication", "https://developers.kakao.com > 내 애플리케이션 > 플랫폼 > Android 플랫폼 추가")
-            android.util.Log.d("MyApplication", "========================================")
-        } catch (e: Exception) {
-            android.util.Log.e("MyApplication", "해시값 출력 실패", e)
-        }
-    }
+
 }
